7c744676e6e77bb2466e0ae8b6d6c54ceab65bdf
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-12 22:42:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const io = require("socket.io")(process.env.PORT || 8888, {
 const rooms = [];
 
 io.on("connection", (socket) => {
-  socket.disconnect();
-  return;
+  // socket.disconnect();
+  // return;
   let currentRoom = "";
   let started = false;
 
