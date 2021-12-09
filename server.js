2e01c1371ab92b44f025e65e1e513b8e248cc87a
Hazim <hazimarafa80@gmail.com>
2021-12-09 15:17:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(process.env.PORT || 3001, {
   cors: {
-    origin: ["http://localhost:3000", "https://tic.webdefy.tech/"],
+    origin: ["http://localhost:3000", "https://tic.webdefy.tech"],
   },
 });
 
